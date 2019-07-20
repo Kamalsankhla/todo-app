4f340f6fc51a62417cb98cb6f554e6f8463c6ee9
Kamalsankhla <kksskk1996@gmail.com>
2019-07-20 18:11:28 +0530

    front end ready in dev

--- src/App.js
+++ src/App.js
@@ -6,16 +6,41 @@ import { BrowserRouter as Router, Route, Link} from "react-router-dom";
 import ToDoCreate from "./components/create-todo.component";
 import ToDoEdit from "./components/edit-todo.component";
 import ToDoList from "./components/list-todo.component";
+import logo from "./logo.jpg";
+
+
 
 class App extends Component {
 render() {
   return(
     <Router>
      <div className= "container">
-      <h2>This is for testing purpose</h2>
-      <Route path='/' exact component = {ToDoList} />
-      <Route path='/edit/:id' exact component = {ToDoEdit} />
-      <Route path='/create' exact component = {ToDoCreate} />
+         <nav className="navbar navbar-expand-lg navbar-light bg-light">
+              <a className="navbar-brand" href="." target="_blank">
+                 <img src={logo} width="30" height="30" alt="chuji"/>         
+              </a>
+              <Link to="/" className="navbar-brand">Blogging Framework</Link>
+              <Link to="/" className="nav-link">ToDoList</Link>
+              <Link to="/edit" className="nav-link">Edit</Link> 
+              <Link to="/create" className="nav-link">Create</Link> 
+               <div className="collapse nav-collapse">
+                  <ul className="navbar-nav mr-auto">
+                     <li className="navbar-item">
+                        <Link to="/" className="nav-link">ToDoList</Link>
+                     </li>
+                     <li className="navbar-item">
+                        <Link to="/edit" className="nav-link">Edit</Link> 
+                     </li> 
+                     <li className="navbar-item">
+                        <Link to="/create" className="nav-link">Create</Link>
+                    </li>
+                 </ul>
+               </div>
+          </nav>
+             
+       <Route path='/' exact component = {ToDoList} />
+       <Route path='/edit/:id' exact component = {ToDoEdit} />
+       <Route path='/create' exact component = {ToDoCreate} />
      </div>
  </Router>
   );
--- src/components/create-todo.component.js
+++ src/components/create-todo.component.js
@@ -1,10 +1,118 @@
 import React, { Component } from "react";
 
 export default class ToDoCreate extends Component{
+    constructor(props){
+        super(props);
+        
+        this.onChangeTodoDescription = this.onChangeTodoDescription.bind(this);
+        this.onChangeTodoResponsibility = this.onChangeTodoResponsibility.bind(this);
+        this.onChangeTodoPriority = this.onChangeTodoPriority.bind(this);
+        this.onSubmit = this.onSubmit.bind(this)
+
+        this.state = { 
+                       todo_description: '',
+                       todo_responsible: '',
+                       todo_priority: '',
+                       todo_completed:''
+        }
+    }
+    
+    onChangeTodoDescription(e){
+        this.setState({
+            todo_description : e.target.value
+        });
+    }
+
+    onChangeTodoResponsibility(e){
+        this.setState({
+            todo_responsible : e.target.value
+        });
+    }
+
+    onChangeTodoPriority(e){
+        this.setState({
+            todo_priority : e.target.value
+        });
+    }
+
+    onSubmit(e){
+                 e.preventDefault();
+                 
+      //           console.log('Form Completed: ${this.state.todo_completed}');
+
+                 this.setState ({
+
+                    todo_description : '',
+                    todo_responsible : '',
+                    todo_priority : '',
+                    todo_completed : ''
+                 });   
+    }
+
     render(){
         return(
-            <div>
-                <p>this is todo create</p>
+            <div style={{marginTop: 20}}>
+                 <h3> Create New Todo List </h3>
+                 <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Description:</label>
+                        <input type="text" 
+                               className="form-control"
+                               value={this.state.todo_description}
+                               onChange={this.onChangeTodoDescription}
+                        />
+                     </div>
+                     <div className="form-group">
+                        <label>Responsibility:</label>
+                        <input type="text" 
+                               className="form-control"
+                               value={this.state.todo_responsible}
+                               onChange={this.onChangeTodoResponsibility}
+                        />
+                     </div>
+                     <div className="form-group">
+                        <label>Priority: </label>
+                        <div className="form-check form-check-inline">
+                            <input type="radio" 
+                                className="form-check-input"
+                                name="priorityOption"
+                                id="priorityLow"
+                                value={this.state.todo_priority==='Low'}
+                                onChange={this.onChangeTodoPriority}
+                            />
+                            <label className="form-check-label">Low</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <input type="radio" 
+                                className="form-check-input"
+                                name="priorityOption"
+                                id="priorityMedium"
+                                value={this.state.todo_priority==='Medium'}
+                                onChange={this.onChangeTodoPriority}
+                            />
+                            <label className="form-check-label">Medium</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <input type="radio" 
+                                className="form-check-input"
+                                name="priorityOption"
+                                id="priorityHigh"
+                                value={this.state.todo_priority==='High'}
+                                onChange={this.onChangeTodoPriority}
+                            />
+                            <label className="form-check-label">High</label>
+                        </div>
+                     </div>
+                     <div className="form-group">
+                        <input type="submit" 
+                               className="btn btn-primary"
+                               value="Create Todo"
+                         />
+                     </div>
+
+                 </form>
+
+                
             </div>
         );
     }
